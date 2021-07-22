32de4a4d5904578fbd2be118bf1832467c875587
Delarta <delarta.tok@gmail.com>
2021-07-22 21:03:14 +0700

    update code

--- index.js
+++ index.js
@@ -3,12 +3,8 @@ const body = document.body;
 
 toggler.onclick = () => {
   if (body.classList.contains("light-theme")) {
-
     body.classList.replace("light-theme", "dark-theme");
-    body.style.backgroundColor = "#494949"
   } else if (body.classList.contains("dark-theme")) {
     body.classList.replace("dark-theme", "light-theme");
-    body.style.backgroundColor = "#494949"
-
   }
 };
